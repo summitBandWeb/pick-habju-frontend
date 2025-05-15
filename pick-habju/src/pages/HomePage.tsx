9c727e745d1bb109a75a18bad06e62cfb45ce6b6
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-05-15 13:56:46 +0900

    ESLint + Prettier + 폴더구조 + Design System 구현 (#6)
    * 🔧 config: 환경변수 설정 및 ESLint 설정과 Prettier 설정 #5
    
    * 🔧 config : Design System 구현 및 도입 #5
    
    * 폴더 구조 유지 #5

---
+++ pick-habju/src/pages/HomePage.tsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className="font-hero-headline text-decoration-hero-edit text-red-400 ">Hello Tailwind with TS Config!</h1>
+      <h1>Hello Tailwind with TS Config!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
