61f103fedecb559eb02a7d21f2cd7ace3fbeef46
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-05-16 19:23:49 +0900

    ✨ feat: PaginationDot Compoents 구현 #7

---
+++ pick-habju/src/components/PaginationDot/PaginationDot.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import type { PaginationDotsProps } from './PaginationDot';
+import PaginationDots from './PaginationDot';
+
+const meta: Meta<PaginationDotsProps> = {
+  title: 'Components/PaginationDots',
+  component: PaginationDots,
+  tags: ['autodocs'],
+  argTypes: {
+    total: {
+      control: { type: 'number', min: 1, step: 1 },
+    },
+    current: {
+      control: { type: 'number', min: 0, step: 1 },
+    },
+  },
+  parameters: {
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#000000' },
+        { name: 'light', value: '#FFFFFF' },
+      ],
+    },
+  },
+};
+export default meta;
+type Story = StoryObj<PaginationDotsProps>;
+
+export const OnePage: Story = {
+  args: { total: 2, current: 0 },
+};
+
+export const TwoPages: Story = {
+  args: { total: 3, current: 0 },
+};
+
+export const ThreePages: Story = {
+  args: { total: 4, current: 0 },
+};
+
+export const FourPages: Story = {
+  args: { total: 5, current: 0 },
+};
---
+++ pick-habju/src/components/PaginationDot/PaginationDot.tsx
@@ -0,0 +1,16 @@
+export interface PaginationDotsProps {
+  total: number;
+  current: number;
+}
+
+const PaginationDots = ({ total, current }: PaginationDotsProps) => (
+  <div className="flex items-center gap-x-[6px]">
+    {Array.from({ length: total }).map((_, idx) => {
+      const isActive = idx === current;
+      const dotClass = isActive ? 'bg-primary-white' : 'bg-gray-300';
+      return <div key={idx} className={`w-2 h-2 rounded-full ${dotClass}`}></div>;
+    })}
+  </div>
+);
+
+export default PaginationDots;
