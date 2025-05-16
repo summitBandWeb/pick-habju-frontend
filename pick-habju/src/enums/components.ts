055bd0c3741b27627ac78f96940e44ed440c9e7e
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-05-16 19:21:29 +0900

    enum 분리 #7

---
+++ pick-habju/src/enums/components.ts
@@ -0,0 +1,22 @@
+// 버튼 관련
+export enum ButtonVariant {
+  Main = 'main',
+  Sub = 'sub',
+  Text = 'text',
+}
+
+// 버튼 Size
+export enum BtnSizeVariant {
+  XSM = 'xsm',
+  SM = 'sm',
+  DEFAULT = 'default',
+  MD = 'md',
+  LG = 'lg',
+}
+
+// Chevron 관련
+export enum ChevronVariant {
+  First = 'first',
+  Middle = 'middle',
+  Last = 'last',
+}
