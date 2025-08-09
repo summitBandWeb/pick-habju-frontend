658effff613f9aa95a073844a8cf74fa44d82c09
kim nan seul <154604350+seulnan@users.noreply.github.com>
2025-08-09 23:59:40 +0900

    [FEAT] NoResult화면개발

--- pick-habju/src/components/Search/SearchSection.tsx
+++ pick-habju/src/components/Search/SearchSection.tsx
@@ -1,16 +1,19 @@
 import { useSearchStore } from '../../store/search/searchStore';
 import { SearchPhase } from '../../store/search/searchStore.types';
 import BeforeSearchView from './BeforeSearchView';
+import NoResultView from './NoResultView';
 
 const SearchSection = () => {
   const { phase } = useSearchStore();
 
-  if (phase === SearchPhase.BeforeSearch) {
-    return <BeforeSearchView />;
+  switch (phase) {
+    case SearchPhase.BeforeSearch:
+      return <BeforeSearchView />;
+    case SearchPhase.NoResult:
+      return <NoResultView />;
+    default:
+      return null; // Loading/Default는 이후 단계에서 구현
   }
-
-  // Loading/NoResult/Default는 이후 단계에서 구현
-  return null;
 };
 
 export default SearchSection;
