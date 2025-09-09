cfe8eca91008979214918b95085f3f18ae8cf1ca
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-09-09 21:00:25 +0900

    [#61[; fix: 370px 로 변경

--- pick-habju/src/components/Search/DefaultSkeletonView.tsx
+++ pick-habju/src/components/Search/DefaultSkeletonView.tsx
@@ -3,7 +3,7 @@ const DefaultSkeletonView = () => {
   return (
     <div className="w-full flex flex-col items-center gap-4 py-4 bg-[#FFFBF0]">
       {Array.from({ length: skeletonCount }).map((_, i) => (
-        <div key={i} className="w-80 h-65 rounded-xl bg-gray-200 animate-pulse" />
+        <div key={i} className="w-92.5 h-65 rounded-xl bg-gray-200 animate-pulse" />
       ))}
     </div>
   );
