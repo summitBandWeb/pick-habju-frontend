94f92abc0854b04004973ea00221c7d452764e5d
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-09-10 22:31:34 +0900

    ♻️ [#66]; refactor: isActive 로직 단순화

--- pick-habju/src/components/PartiallyPossibleButton/PartiallyPossibleButton.tsx
+++ pick-habju/src/components/PartiallyPossibleButton/PartiallyPossibleButton.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import GrayCheckIcon from '../../assets/svg/GrayCheck.svg';
 import BlueCheckIcon from '../../assets/svg/BlueCheck.svg';
 import { useSearchStore } from '../../store/search/searchStore';
@@ -6,14 +5,12 @@ import { useSearchStore } from '../../store/search/searchStore';
 const PartiallyPossibleButton = () => {
   const includePartiallyPossible = useSearchStore((s) => s.includePartiallyPossible);
   const setIncludePartiallyPossible = useSearchStore((s) => s.setIncludePartiallyPossible);
-  const [isFocused, setIsFocused] = useState(false);
 
-  const isActive = includePartiallyPossible && isFocused;
+  const isActive = includePartiallyPossible;
 
   return (
     <button
       className="w-[8.4375rem] h-10 rounded-[6.25rem] shadow-filter px-4 py-2.5 flex gap-2.5 items-center outline-none hover:bg-gray-100"
-      onFocus={() => setIsFocused(true)}
       onClick={() => setIncludePartiallyPossible(!includePartiallyPossible)}
     >
       <span className={`font-modal-call ${isActive ? 'text-blue-500' : 'text-gray-300'}`}>부분가능 포함</span>
