569d0bd854c49fc1f191a59e134dced0c5446f9d
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-09-11 18:57:19 +0900

    ✨ [#65]; feat: GoogleFormToast 적용

--- pick-habju/src/components/Header/AppHeader.tsx
+++ pick-habju/src/components/Header/AppHeader.tsx
@@ -1,4 +1,5 @@
 import HeaderLogo from './HeaderLogo';
+import GoogleFormToast from '../GoogleFormToast/GoogleFormToast';
 
 // const SafeArea = () => (
 //   <div
@@ -8,17 +9,15 @@ import HeaderLogo from './HeaderLogo';
 // );
 
 const LogoHeader = () => (
-  <div
-    className="flex items-center self-stretch bg-primary-white py-[0.375rem] pr-4"
-    aria-label="Logo Header"
-  >
+  <div className="flex items-center self-stretch bg-primary-white py-[0.375rem] pr-4" aria-label="Logo Header">
     <HeaderLogo />
   </div>
 );
 
 const AppHeader = () => {
   return (
-    <div className="flex w-full justify-center items-center">
+    <div className="flex w-full justify-center items-center relative">
+      <GoogleFormToast />
       <div className="flex flex-col w-full max-w-[25.9375rem]">
         {/* <SafeArea /> */}
         <LogoHeader />
