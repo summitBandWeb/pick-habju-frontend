a791cf26898b1f8feb0f2cbb0057a53facd7789e
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2025-09-15 21:17:58 +0900

    💄 [#81]; ui: 부분가능 포함 -> 일부시간 가능만

--- pick-habju/src/components/PartiallyPossibleButton/PartiallyPossibleButton.tsx
+++ pick-habju/src/components/PartiallyPossibleButton/PartiallyPossibleButton.tsx
@@ -10,10 +10,10 @@ const PartiallyPossibleButton = () => {
 
   return (
     <button
-      className="w-[8.4375rem] h-10 rounded-[6.25rem] shadow-filter px-4 py-2.5 flex gap-2.5 items-center outline-none bg-primary-white hover:bg-gray-100"
+      className="w-[9.25rem] h-10 rounded-[6.25rem] shadow-filter px-4 py-2.5 flex gap-2.5 items-center outline-none bg-primary-white hover:bg-gray-100"
       onClick={() => setIncludePartiallyPossible(!includePartiallyPossible)}
     >
-      <span className={`font-modal-call ${isActive ? 'text-blue-500' : 'text-gray-300'}`}>부분가능 포함</span>
+      <span className={`font-modal-call ${isActive ? 'text-blue-500' : 'text-gray-300'}`}>일부시간 가능만</span>
       <img src={isActive ? BlueCheckIcon : GrayCheckIcon} alt="check icon" />
     </button>
   );
