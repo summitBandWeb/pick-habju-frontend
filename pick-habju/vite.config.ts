fd77bf561a995fc1d1189a42ef75b33becc312c1
sandocsol <jungan1030@gmail.com>
2026-01-30 15:21:48 +0900

    � package:vite-plugin-svgr 설치 및 설정

--- pick-habju/vite.config.ts
+++ pick-habju/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
+import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  plugins: [tailwindcss(), svgr()],
 });
