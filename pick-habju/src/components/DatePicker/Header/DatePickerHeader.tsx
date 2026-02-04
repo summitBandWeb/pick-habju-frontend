8eaaf33297b3c30e94b5bf78d073a321e0c5c9cc
sandocsol <jungan1030@gmail.com>
2026-02-04 16:14:47 +0900

    ui: DatePickerHeader 요소 크기, 패딩 수정

--- pick-habju/src/components/DatePicker/Header/DatePickerHeader.tsx
+++ pick-habju/src/components/DatePicker/Header/DatePickerHeader.tsx
@@ -3,15 +3,15 @@ import type { DatePickerHeaderProps } from './DatePickerHeader.types';
 
 const DatePickerHeader = ({ current, onPrev, onNext }: DatePickerHeaderProps) => (
   <div className="flex items-center justify-between px-4 py-2 bg-white rounded-t-lg">
-    <button onClick={onPrev} aria-label="이전 달" className="font-modal-num text-primary-black">
-      <ChevronLeftIcon />
+    <button onClick={onPrev} aria-label="이전 달" className="p-[0.63rem] font-modal-num text-primary-black">
+      <ChevronLeftIcon className="w-7 h-7" />
     </button>
-    <div className="flex items-center space-x-2 font-modal-default text-primary-black">
-      <span>{current.getFullYear()}년</span>
-      <span>{current.getMonth() + 1}월</span>
+    <div className="flex items-center font-modal-default text-primary-black">
+      <span className="py-3 px-1">{current.getFullYear()}년</span>
+      <span className="py-3 px-1">{current.getMonth() + 1}월</span>
     </div>
-    <button onClick={onNext} aria-label="다음 달" className="font-modal-default text-primary-black">
-      <ChevronRightIcon />
+    <button onClick={onNext} aria-label="다음 달" className="p-[0.63rem] font-modal-default text-primary-black">
+      <ChevronRightIcon className="w-7 h-7" />
     </button>
   </div>
 );
