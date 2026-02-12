7a3331741fb4d227e9f436ac1a3a8736cbd7e684
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2026-02-12 17:34:46 +0900

    📦️ [#127]; package: ky, tanstack query 설치

--- pick-habju/src/main.tsx
+++ pick-habju/src/main.tsx
@@ -2,15 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useSessionAnalyticsStore } from './store/analytics/sessionStore';
 if (import.meta.env.DEV && import.meta.env.VITE_USE_MSW === 'true') {
   import('./mocks/browser').then(({ startMockWorker }) => startMockWorker());
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <StrictMode>
+      <App />
+      {import.meta.env.DEV ? <ReactQueryDevtools initialIsOpen={false} /> : null}
+    </StrictMode>
+  </QueryClientProvider>
 );
 
 // 세션 종료 시 요약 이벤트 전송
