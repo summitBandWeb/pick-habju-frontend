2c2c730f1d4fff97be6149db10512d7bc532affa
sandocsol <jungan1030@gmail.com>
2026-03-03 06:17:03 +0900

    �️  [#150]; hotfix:min_price API 응답구조 수정에 따른 코드 수정

--- pick-habju/src/api/api.types.ts
+++ pick-habju/src/api/api.types.ts
@@ -40,7 +40,8 @@ export interface Branch {
   branch: string;
   lat: number;
   lng: number;
-  min_price: number;
+  min_price_available: number;
+  min_price_partial: number;
   available_count: number;
   phone_number: string | null;
   display_name: string | null;
