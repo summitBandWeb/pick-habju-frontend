80f10d638a686c3af5b87111da00f1318fe95b06
S-Gihun <89789115+S-Gihun@users.noreply.github.com>
2026-03-04 02:42:38 +0900

    🔧 [#161]; fix: 스타일 일관성 및 공식 표기 활용

--- pick-habju/src/components/Header/AppHeader.tsx
+++ pick-habju/src/components/Header/AppHeader.tsx
@@ -18,7 +18,7 @@ const AppHeader = () => {
   return (
     <div className="flex w-full justify-center items-center relative">
       <GoogleFormToast />
-      <div className="flex flex-col w-full max-w-100.5">
+      <div className="flex flex-col w-full max-w-[25.125rem]">
         {/* <SafeArea /> */}
         <LogoHeader />
       </div>
